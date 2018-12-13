58b3a7a5a34a4e0244828c30cb4b24c27015e5c7
Fernando Franzim <franzim@fedora.home>
2018-12-12 23:05:32 -0200

    Aula 4 - parte da aula - Helpers e metodos estaticos.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -10,44 +10,14 @@ class NegociacaoController {
     add(event) {
         event.preventDefault();
 
-        console.log(typeof (this._inputData.value));
-        console.log(this._inputData.value);
-
-        let dataExpressaoRegular = new Date(this._inputData.value.replace(/-/g, '-'));
-        console.log('dataExpressaoRegular: ' + dataExpressaoRegular);
-        let dataExpressaoMuitoLouca = new Date(
-            ...this._inputData.value.split('-').map(function (item, indice) {
-                if (indice == 1) {
-                    return (item - 1);
-                }
-                return item;
-            }));
-
-        console.log('dataExpressaoMuitoLouca: ' + dataExpressaoMuitoLouca);
-
-        let dataExpressaoMuitoLoucaArrowFunction = new Date(
-            ...this._inputData.value.split('-').map((item, indice) => {
-                (item - indice % 2)
-            }));
-        console.log('dataExpressaoMuitoLoucaArrowFunction: ' + dataExpressaoMuitoLouca);
-
-        let dataExpressaoMuitoLoucaArrowFunctionUmaUnicaExpressaoOmiteBloco =
-            new Date(
-                ...this._inputData.value.split('-').map(
-                    (item, indice) => (item - indice % 2)
-                )
-            );
-        console.log('dataExpressaoMuitoLoucaArrowFunctionUmaUnicaExpressaoOmiteBloco: ' + dataExpressaoMuitoLouca);
-
-
         let negociacao = new Negociacao(
-            new Date(this._inputData.value.split('-')),
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQtd.value,
             this._inputValor.value
         );
 
         console.log(negociacao);
-        // adiciona negociação em uma lista
+        console.log(DateHelper.dataParaTexto(negociacao.data));
 
     }
 
